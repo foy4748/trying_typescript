f436540be8178ad8f9c86edde3794cd92f81ebd9
f-rahman23 <f.rahman@iideainformatics.com>
2023-08-14 00:07:07 +0600

    Removed console.log s

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -1,94 +1,92 @@
-import { useReducer } from "react";
+import {useReducer} from "react";
 import Button from "../StyledComponents/Button";
 
 type CounterType = {
-  count: number;
+	count: number;
 };
 
 type CounterResetActionType = {
-  type: "reset";
+	type: "reset";
 };
 
 type CounterUpdateActionType = {
-  payload: CounterType;
-  type: "increase" | "decrease";
+	payload: CounterType;
+	type: "increase" | "decrease";
 };
 
 type CounterActionType = CounterUpdateActionType | CounterResetActionType;
 
 export default function Counter() {
-  // Counter Reducer Setup
-  const initialCount: CounterType = {
-    count: 0,
-  };
-  const counterReducer = (state: CounterType, action: CounterActionType) => {
-    console.log(action);
-    switch (action.type) {
-      case "increase":
-        return { count: state.count + action.payload.count };
-      case "decrease":
-        return { count: state.count - action.payload.count };
-      case "reset":
-        return { count: 0 };
-      default:
-        return state;
-    }
-  };
+	// Counter Reducer Setup
+	const initialCount: CounterType = {
+		count: 0,
+	};
+	const counterReducer = (state: CounterType, action: CounterActionType) => {
+		switch (action.type) {
+			case "increase":
+				return {count: state.count + action.payload.count};
+			case "decrease":
+				return {count: state.count - action.payload.count};
+			case "reset":
+				return {count: 0};
+			default:
+				return state;
+		}
+	};
 
-  const [count, dispatchCount] = useReducer(counterReducer, initialCount);
+	const [count, dispatchCount] = useReducer(counterReducer, initialCount);
 
-  // Event Handlers
-  const increaseCount = (
-    event: React.MouseEvent<HTMLButtonElement>,
-    increaseBy: number
-  ) => {
-    console.log(event.target);
-    dispatchCount({ type: "increase", payload: { count: increaseBy } });
-  };
-  const decreaseCount = (
-    event: React.MouseEvent<HTMLButtonElement>,
-    decreaseBy: number
-  ) => {
-    console.log(event.target);
-    dispatchCount({ type: "decrease", payload: { count: decreaseBy } });
-  };
+	// Event Handlers
+	const increaseCount = (
+		event: React.MouseEvent<HTMLButtonElement>,
+		increaseBy: number
+	) => {
+		dispatchCount({type: "increase", payload: {count: increaseBy}});
+	};
+	const decreaseCount = (
+		event: React.MouseEvent<HTMLButtonElement>,
+		decreaseBy: number
+	) => {
+		console.log(event.target);
+		dispatchCount({type: "decrease", payload: {count: decreaseBy}});
+	};
 
-  return (
-    <section>
-      <h1 className="text-3xl">{count.count}</h1>
-      <div>
-        <Button
-          onClick={(e: React.MouseEvent<HTMLButtonElement>): void =>
-            increaseCount(e, 1)
-          }
-        >
-          Increase
-        </Button>
-        <Button
-          onClick={(e: React.MouseEvent<HTMLButtonElement>): void =>
-            decreaseCount(e, 1)
-          }
-        >
-          Decrease
-        </Button>
-      </div>
-      <div>
-        <Button
-          onClick={(e: React.MouseEvent<HTMLButtonElement>): void =>
-            increaseCount(e, 5)
-          }
-        >
-          Increase by 5
-        </Button>
-        <Button
-          onClick={(e: React.MouseEvent<HTMLButtonElement>): void =>
-            decreaseCount(e, 5)
-          }
-        >
-          Decrease by 5
-        </Button>
-      </div>
-      <Button onClick={() => dispatchCount({ type: "reset" })}>Reset</Button>
-    </section>
-  );
+	return (
+		<section>
+			<h1 className="text-3xl">{count.count}</h1>
+			<div>
+				<Button
+					onClick={(e: React.MouseEvent<HTMLButtonElement>): void =>
+						increaseCount(e, 1)
+					}
+				>
+					Increase
+				</Button>
+				<Button
+					onClick={(e: React.MouseEvent<HTMLButtonElement>): void =>
+						decreaseCount(e, 1)
+					}
+				>
+					Decrease
+				</Button>
+			</div>
+			<div>
+				<Button
+					onClick={(e: React.MouseEvent<HTMLButtonElement>): void =>
+						increaseCount(e, 5)
+					}
+				>
+					Increase by 5
+				</Button>
+				<Button
+					onClick={(e: React.MouseEvent<HTMLButtonElement>): void =>
+						decreaseCount(e, 5)
+					}
+				>
+					Decrease by 5
+				</Button>
+			</div>
+			<Button onClick={() => dispatchCount({type: "reset"})}>Reset</Button>
+		</section>
+	);
 }
--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -1,14 +1,40 @@
-import {useContext} from "react";
+import {useContext, useEffect, useRef, useState} from "react";
 import {UserContext} from "../Contexts/UserContext";
 import Button from "../StyledComponents/Button";
 
 export default function Dashboard() {
 	const {isLoggedIn, setIsLoggedIn} = useContext(UserContext);
-	console.log(isLoggedIn)
+	const [timer, setTimer] = useState(0)
+
+	// Added null! , if you are sure your DOM is always truthy
+	const someRef = useRef<HTMLParagraphElement>(null!)
+	const someTimer = useRef<number | null>(null);
+	const stopTimer = () => {
+		if (someTimer.current) window.clearInterval(someTimer.current);
+	}
+
+	useEffect(() => {
+		window.setInterval(() => {
+			setTimer(curr => curr + 1);
+		}, 1000)
+		return stopTimer;
+	}, [])
+
+	useEffect(() => {
+		if (isLoggedIn) {
+			someRef.current.style.backgroundColor = 'blue';
+			someRef.current.style.color = '#fff';
+		} else {
+
+			someRef.current.style.backgroundColor = 'red';
+			someRef.current.style.color = '#fff';
+		}
+	}, [isLoggedIn])
 
 	return (
 		<section>
-			<p>{isLoggedIn ? 'Logged In' : 'Not Logged In'}</p>
+			<h2>{timer}</h2>
+			<p ref={someRef}>{isLoggedIn ? 'Logged In' : 'Not Logged In'}</p>
 			<Button onClick={() => setIsLoggedIn((curr) => !curr)}>Testing</Button>
 		</section>
 	)
